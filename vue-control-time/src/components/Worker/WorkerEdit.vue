04ba9b1d9d65b5b2a5f05c0ead182881754d4b5a
artem <temicska99@mail.ru>
2020-01-14 20:22:12 +0200

    Vue: сотрудник: CRUD

---
+++ vue-control-time/src/components/Worker/WorkerEdit.vue
@@ -0,0 +1,141 @@
+<template>
+    <mdb-card v-if="worker.id > 0">
+        <mdb-card-body>
+            <mdb-card-title>Сотрудник: {{ worker.name }} {{ worker.family }}</mdb-card-title>
+
+            <form @submit="checkForm">
+                <mdb-card-text>
+                    <mdb-input label="Имя" v-model="worker.name" />
+                    <mdb-input label="Отчество" v-model="worker.patronymic" />
+                    <mdb-input label="Фамилия" v-model="worker.family" />
+                    <mdb-input label="Номер телефона" v-model="worker.phone" />
+                </mdb-card-text>
+                <mdb-btn style="width:100%" color="default">Сохранить</mdb-btn>
+            </form>
+            <router-link v-bind:to="linkList">
+                <mdb-btn style="width:100%" color="primary">Открыть список сотрудников</mdb-btn>
+            </router-link>
+        </mdb-card-body>
+
+        <ModalResult v-if="isOpenModalResult" v-bind:result="resultSave" v-bind:form-errors="formErrors"
+                         @closeModal="closeModalResult" v-bind:back-link=linkList />
+    </mdb-card>
+</template>
+
+<script>
+    import { mdbCard, mdbCardImage, mdbCardBody, mdbCardTitle, mdbCardText, mdbBtn, mdbInput} from 'mdbvue';
+    import axios from 'axios';
+    import ModalResult from "../ModalResult";
+    export default {
+        components: {
+            mdbCard,
+            mdbCardImage,
+            mdbCardBody,
+            mdbCardTitle,
+            mdbCardText,
+            mdbBtn,
+            mdbInput,
+            ModalResult
+        },
+
+        data() {
+            const blankWorker = {
+                id: 0,
+                firstName: null,
+                patronymic: null,
+                family: null,
+                phone: null
+            };
+
+            return {
+                error: null,
+                isOpenModalResult: false,
+                resultSave: null,
+                formErrors: [],
+                linkList: '/workers/all',
+                workerId: this.$route.params.id,
+                worker: blankWorker
+            }
+        },
+
+        methods: {
+            save() {
+                this.resultSave = null;
+
+                const options = {
+                  'entryData': this.worker
+                };
+
+                axios.put(API_URL + '/worker/' + this.workerId, options)
+                    .then((response) => {
+                        if (response.data.success) {
+                            this.resultSave = 'Данные успешно сохранены!';
+                            this.worker = response.data.entryData;
+                        } else {
+                            this.resultSave = response.data.message;
+                        }
+                    })
+                    .catch((error) => {
+                        this.resultSave = error;
+                    })
+                    .finally(() => (this.isOpenModalResult = true));
+            },
+            loadWorker() {
+                axios.get(API_URL + '/worker/' + this.workerId)
+                    .then(response => {
+                        this.worker = response.data;
+                    })
+                    .catch(e => {
+                        this.error = e;
+                    }).finally(() => (document.title = 'Сотрудник: ' + this.worker.name + ' ' + this.worker.family));
+            },
+            checkForm(e) {
+                this.formErrors = [];
+                this.resultSave = '';
+
+                if (!this.worker.name) {
+                    this.formErrors.push('Не указано имя');
+                }
+
+                if (!this.worker.patronymic) {
+                    this.formErrors.push('Не указано отчество');
+                }
+
+                if (!this.worker.family) {
+                    this.formErrors.push('Не указана фамилия');
+                }
+
+                if (!this.isValidPhone()) {
+                    this.formErrors.push('Не корректно указан номер');
+                }
+
+                if (!this.formErrors.length) {
+                    this.save();
+                } else {
+                    this.isOpenModalResult = true;
+                }
+
+                e.preventDefault();
+            },
+            isValidPhone() {
+                if (!this.worker.phone || this.worker.phone.length !== 11) {
+                    return false;
+                }
+
+                let intPhone = parseInt(this.worker.phone);
+                if (this.worker.phone != intPhone) {
+                    return false;
+                }
+
+                return true;
+            },
+            closeModalResult() {
+                this.isOpenModalResult = false;
+            }
+        },
+
+        created() {
+            this.loadWorker();
+        }
+    }
+</script>
---
+++ vue-control-time/src/components/Worker/WorkerList.vue
@@ -0,0 +1,118 @@
+<template>
+    <mdb-card>
+        <mdb-card-body>
+            <mdb-card-title>Список сотрудников</mdb-card-title>
+            <mdb-card-text>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Фамилия</th>
+                        <th scope="col">Имя</th>
+                        <th scope="col">Отчество</th>
+                        <th scope="col">Телефон</th>
+                        <th scope="col">Дата принятия на работу</th>
+                        <th scope="col">Стаж</th>
+                        <th scope="col">Действия</th>
+                    </tr>
+                    </thead>
+                    <tr v-for="item in workers">
+                        <td>{{ item.id }}</td>
+                        <td>{{ item.family }}</td>
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.patronymic }}</td>
+                        <td>
+                            <span v-if="item.phone">{{ item.phone | renderPhone }}</span>
+                            <mdb-badge color="danger" v-else>не указан</mdb-badge>
+                        </td>
+                        <td>
+                            {{ item.hired_date }}
+                        </td>
+                        <td>
+                            {{ item.hired_date | renderExperience}}
+                        </td>
+                        <td>
+                            <a class="far fa-trash-alt" @click="removeWorker(item.id)" title="Удалить сотрудника"></a>
+
+                            <router-link :to="{ name: 'workerEdit', params: { id: item.id }}">
+                                <i class="far fa-edit ml-2" title="Редактировать"></i>
+                            </router-link>
+                        </td>
+                    </tr>
+                </table>
+            </mdb-card-text>
+
+            <Pagination v-bind:totalCount="totalCount" @openPage="refreshWorkers" v-if="isLoadEntries"/>
+        </mdb-card-body>
+    </mdb-card>
+</template>
+
+<script>
+    import { mdbCard, mdbCardImage, mdbCardBody, mdbCardTitle, mdbCardText, mdbBtn, mdbBadge } from 'mdbvue';
+    import axios from 'axios';
+    import Pagination from '@/components/Pagination';
+    export default {
+        data() {
+            return {
+                workers: [],
+                error: null,
+                totalCount: null,
+                maxCountEntriesForOnePage: 10,
+                isLoadEntries: false,
+                currentOffset: 0,
+                currentPage: 1
+            }
+        },
+
+        components: {
+            mdbCard,
+            mdbCardImage,
+            mdbCardBody,
+            mdbCardTitle,
+            mdbCardText,
+            mdbBtn,
+            Pagination,
+            mdbBadge
+        },
+
+        created() {
+            this.loadWorkers(1);
+
+            document.title = 'Список сотрудников';
+        },
+
+        methods: {
+            loadWorkers(page) {
+                if (page === undefined) {
+                    page = this.currentPage;
+                }
+
+                const URL = API_URL + '/worker/page-' + page + '/sort/id-asc';
+
+                axios.get(URL)
+                    .then(response => {
+                        this.workers = response.data.data;
+                        this.totalCount = response.data.total;
+                        this.isLoadEntries = true;
+
+                        this.currentPage = page;
+                    })
+                    .catch(e => {
+                        this.error=e;
+                    });
+            },
+            refreshWorkers(page) {
+                this.loadWorkers(page);
+            },
+            removeWorker(id) {
+                axios.delete(API_URL + '/worker/' + id)
+                    .then(response => {
+                        this.refreshWorkers();
+                    })
+                    .catch(e => {
+                        this.error=e;
+                    });
+            }
+        }
+    }
+</script>
