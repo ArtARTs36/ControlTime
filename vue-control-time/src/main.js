51d44d288a87872157d054d4ee1edd1379c73c2f
aukrainskiy <aukrainskiy@phoenixit.ru>
2020-05-30 03:38:11 +0300

    api_url

--- vue-control-time/src/main.js
+++ vue-control-time/src/main.js
@@ -7,7 +7,7 @@ import './registerServiceWorker'
 import router from './router'
 import 'bootstrap-css-only/css/bootstrap.min.css';
 
-window.API_URL = 'http://localhost:8888/api';
+window.API_URL = '//'+ window.location.hostname + ':8888/api';
 
 const daysOfWeek = [
     'Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'
